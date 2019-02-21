305cdd3ac639a9a79896b3a6cc8dbc35a7424645
Kumar <nishant.am.kumar@accenture.com>
2019-02-21 21:02:26 +0530

    modified/added new UI methods

--- CanSolV2-master/view.py
+++ CanSolV2-master/view.py
@@ -1,25 +1,30 @@
 from tkinter import *
 
 class UI:
+    #browses the raw data set.
     def browse(self):
         print("Clicked :::browsing and selecting raw data the UI....")
 
-    def train(self):
-        print("Clicked :::training data using Scikit....")
+    #renders the plot( confusion graph) and Accuracy
+    def plotAccuracy(self):
+        print("Clicked :::predicting accuracy % & plotting based on trained deployed data...")
 
-    def accuracy(self):
-        print("Clicked :::rendering accuracy/ploting based on trained deployed data....")
-
+     #renders the Benign Malignant confidence score based on the engine
     def predict(self):
-        print("Clicked :::predicting/plotting based on trained deployed data...")
+        print("Clicked :::predicting Malignant - Benign confidence score on UI")
+
+    # resets the plot and accuracy
+    def reset(self):
+        print("Clicked :::reseting plot and accuracy from UI...")
 
     def invoke(self):
         master = Tk()
-        master.title('Cansol')
+        master.title('CanSol')
         e1 = Entry(master)
         e1.grid(row=0, column=1)
         e1.insert(0, "Cancer-Usecase DataSet.csv")
-        Button(master, text='Browse',command=self.browse).grid(row=0, column=2, sticky=W, pady=2)
-        Button(master, text='Train', command=self.train).grid(row=1, column=0, sticky=W, pady=2)
-        Button(master, text='Deploy',command=self.accuracy).grid(row=1, column=1, sticky=W, pady=2)
+        Button(master, text='Browse',command=self.browse).grid(  row=0,    column=2,   sticky=W,   pady=2)
+        Button(master, text='Plot-Accuracy', command=self.plotAccuracy).grid(   row=1,    column=0,   sticky=W,   pady=2)
+        Button(master, text='Reset',command=self.reset).grid(row=1,    column=2,   sticky=W,   pady=2)
+        Button(master, text='Predict', command=self.predict).grid(row=3, column=2, sticky=W, pady=2)
         mainloop( )
